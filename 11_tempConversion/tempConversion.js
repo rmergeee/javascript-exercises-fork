896894d796f6dd60cf4078b4d7739fda417621e8
rmergeee <63661366+rmergeee@users.noreply.github.com>
2025-06-22 00:20:59 +0300

    complete ex 10-11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperaturaF) {
+  const temperaturaC = (temperaturaF - 32) * 5 / 9;
+  return Math.round(temperaturaC * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperaturaC) {
+  const temperaturaF = temperaturaC * 9 / 5 + 32;
+  return Math.round(temperaturaF * 10) / 10;
 };
 
 // Do not edit below this line
