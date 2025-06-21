9b1abae4b1e26ef2737577eae801629f22ce54a9
rmergeee <63661366+rmergeee@users.noreply.github.com>
2025-06-21 21:24:23 +0300

    complete ex 6-9

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...items) {
+    for (const item of items) {
+        for (let i = 0; i < arr.length; i++ ) {
+            let index = arr.findIndex(it => it === item);
+            console.log(index);
+            if(index >= 0) arr.splice(index, 1);    
+        }
+    
+    }
+    return arr;
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, lastNumber) {
+    if (Number.isInteger(firstNumber) 
+        && Number.isInteger(lastNumber)
+        && firstNumber >=0
+        && lastNumber >= 0) {
+        let sumAll = 0;
+        if(lastNumber < firstNumber) {
+            for( let i = lastNumber; i <= firstNumber; i++) {
+            sumAll += i;
+            }
+        }
+        for( let i = firstNumber; i <= lastNumber; i++) {
+            sumAll += i;
+        }
+        return sumAll;
+        
+    } else {
+        return "ERROR";
+    }
 
-};
+    }
+    
 
 // Do not edit below this line
 module.exports = sumAll;
