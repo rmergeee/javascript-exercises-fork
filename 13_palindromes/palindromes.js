6c836148f4311d5bde5cc028d7ec4d9058a3399e
rmergeee <63661366+rmergeee@users.noreply.github.com>
2025-06-27 16:22:30 +0300

    complete palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let regex = /!|\.|,| /gi;
+    let cleanString = string.replace(regex, "").toLowerCase();
+    let newString = cleanString.toLowerCase().split("").reverse().join("");
+    if (newString === cleanString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
+palindromes("gd  shjhk k,wfw    e!");
 // Do not edit below this line
 module.exports = palindromes;
