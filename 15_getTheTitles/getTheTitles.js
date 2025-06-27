393f74ef3c444fa9908fb57f8b4060e22b5cb3eb
rmergeee <63661366+rmergeee@users.noreply.github.com>
2025-06-27 22:30:26 +0300

    complete getTheTitles

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    return books.reduce((arr, book) => {
+        arr.push(book.title);
+        return arr;
+    }, [])
 };
 
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
+
+console.log(getTheTitles(books));
 // Do not edit below this line
 module.exports = getTheTitles;
