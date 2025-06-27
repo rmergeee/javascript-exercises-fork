7a89888f8831344ef59f80c98a5715650dcfb316
rmergeee <63661366+rmergeee@users.noreply.github.com>
2025-06-27 21:59:54 +0300

    complete fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let fibonacci = [1, 1];
+    if (number === 0) return 0;
+    if (number < 0) return "OOPS";
+    if (number === "0") return 0;
+    for(let i = 1; i <= number - 2; i++ ) {
+        fibonacci.push(fibonacci[fibonacci.length - 2] + fibonacci[fibonacci.length - 1]);
+    }
+    return fibonacci[fibonacci.length - 1];
 };
 
+console.log(fibonacci("6"));
 // Do not edit below this line
 module.exports = fibonacci;
