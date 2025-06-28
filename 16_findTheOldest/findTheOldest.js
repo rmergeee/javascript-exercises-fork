2b437493747d72ce0333bef717bd86d303abe67d
rmergeee <63661366+rmergeee@users.noreply.github.com>
2025-06-28 15:16:05 +0300

    complete findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,43 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    const editPeople = people.map(function(obj) {
+        if ("yearOfDeath" in obj) {
+            return {name: obj.name, years: obj.yearOfDeath - obj.yearOfBirth};
+        }else {
+            return {name: obj.name, years: currentYear - obj.yearOfBirth};
+        }
+    })
+    const oldest = editPeople.reduce((prev, curr) => {
+        if (prev.years > curr.years) {
+            return prev;
+        } else {
+            return curr;
+        }
+    })
+    
+    return oldest;
 };
+
+const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
+
+
+console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
